a4edd4c36f114b74d537bccb4a32152db2ebd120
Charles Stockman <chuck.stockman@yahoo.com>
2021-01-08 22:11:54 -0500

    Using the reuse library

--- distro/arch/commands.sh
+++ distro/arch/commands.sh
@@ -13,33 +13,6 @@ function updateWorld {
 }
 
 #
-# Installs software from different package installers
-#    Parameter 1 : The executable for the package manager
-#    Parameter 2 : The associative array containing the package names to be extracted.
-#    Parameter 3 : A string that desbribe the type of packages being extracted.
-#
-function installs {
-  local -n packages=$2
-  echo "Installing " $3
-  for packageGroupKey in "${!packages[@]}"
-    do
-      for package in ${packages[$packageGroupKey]}
-      do
-        $1 $package
-      done
-    done
-
-}
-
-#
-# Install all software package groups
-#
-function installAll {
-  installs install softwareList "softwareList"
-  installs installPip pipList "pipList"
-}
-
-#
 # Install a specific package
 #
 # 	-S 				install the package
@@ -47,13 +20,6 @@ function installAll {
 #
 function install {
 	pacman -S --noconfirm $1
-}
-
-#
-# Function install python libraries and applications
-#
-function installPip {
-  pip install $1
 }
 
 #
@@ -72,16 +38,6 @@ function installNvidia {
 	mhwd -a pci nonfree 0300
 	nvidia-modprobe
 	reboot
-}
-
-
-#
-# Access the AUR using GUI  
-#
-# dependencies : pamac
-# 
-function installAurGui() {
-	install pamac-gtk
 }
 
 #
